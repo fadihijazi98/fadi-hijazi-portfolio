ff6e103a668008b57bcf113fed57ae7994632721
fadihijazi <fadi.harri@gmail.com>
2022-10-13 16:48:44 +0300

    HTML layout of portfolio

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="flex">
+    <!-- custom left fixed header -->
+    <header>
+      <div>
+        <!-- image -->
+      </div>
+      <nav>
+        <router-link to="/" active>Home</router-link>
+        <router-link to="/">Experience</router-link>
+        <router-link to="/">Skills</router-link>
+        <router-link to="/">Get in <br />Touch</router-link>
+      </nav>
+    </header>
+    <!-- content -->
+    <main>
+      <router-view />
+    </main>
+  </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
